7870a6426a7c424ae48d6f9c75fa6cb4ea174fe6
Daniel <dansanrjsdev@hotmail.com>
2022-12-26 14:03:22 -0500

    Codigos de estado o HTTP response status codes

--- routes/products.router.js
+++ routes/products.router.js
@@ -28,16 +28,23 @@ router.get('/filter', (req, res) => {
 //Los : indican que son un parametro
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
-  })
+  // Todos los parametros que se reciban por get los va a enviar como string
+  if (id === '999') {
+    res.status(404).json({
+      message: 'not found'
+    })
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Product 2',
+      price: 2000
+    });
+  }
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   })
