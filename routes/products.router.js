70c7a8fcdb065e3d014c246919d2b39c1fea34d9
Daniel <dansanrjsdev@hotmail.com>
2022-12-31 12:50:48 -0500

    Probando nuestros endpoints

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const ProductsService = require('./../services/product.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema } = require('./../schemas/product.schema');
 
 const router = express.Router();
 const service = new ProductsService();
@@ -18,7 +20,9 @@ router.get('/filter', (req, res) => {
 
 // Endpoint dinamico
 //Los : indican que son un parametro
-router.get('/:id', async (req, res, next) => {
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
   try {
     const { id } = req.params;
     const product = await service.findOne(id);
@@ -28,7 +32,9 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
   const body = req.body;
   const newProduct = await service.create(body);
   res.status(201).json(newProduct);
@@ -36,7 +42,10 @@ router.post('/', async (req, res) => {
 
 // Recibimos el id del producto a editar
 // Con put es igual, solo se cambia patch por put
-router.patch('/:id', async (req, res, next) => {
+router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
   try {
     const { id } = req.params;
     const body = req.body;
